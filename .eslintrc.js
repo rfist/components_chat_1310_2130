1ee5923318e6c38099991236591106a6f5fe6c12
Waldemar.Bud <waldemar.bud@speedandfunction.com>
2017-12-05 19:36:22 +0200

    add esling config

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    'extends': 'google',
+    'parserOptions': {
+        'ecmaVersion': 6,
+        'sourceType': 'module'
+    },
+    'rules': {
+        'comma-dangle': [2, 'never'],
+        'require-jsdoc': 0,
+        'max-len': [1, 140]
+    }
+}
